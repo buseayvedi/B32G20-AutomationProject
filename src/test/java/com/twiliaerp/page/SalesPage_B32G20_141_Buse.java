1490548e6cdf2c7a90006d35190a010296cbac0e
Buse Ayvedi <146566700+buseayvedi@users.noreply.github.com>
2024-03-19 22:05:42 -0400

    add SalesPage_B32G20_141_Buse.java

---
+++ src/test/java/com/twiliaerp/page/SalesPage_B32G20_141_Buse.java
@@ -0,0 +1,31 @@
+package com.twiliaerp.page;
+
+import com.twiliaerp.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class SalesPage_B32G20_141_Buse extends BasePage_US01_Buse {
+
+    public SalesPage_B32G20_141_Buse(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(css = "input.o_searchview_input")
+    public WebElement searchBox;
+
+    @FindBy(css = "th.o_column_sortable")
+    public List<WebElement> listTitles_QuotationsPage;
+
+    @FindBy(css = "td[class*='o_data_cell']")
+    public List<WebElement> searchResultElements;
+
+    public void clickSearchBox(){
+        searchBox.click();
+    }
+
+
+
+}

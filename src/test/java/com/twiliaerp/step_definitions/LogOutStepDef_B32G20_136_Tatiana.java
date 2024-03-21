8971ba6e6b05c5b821808a06fc8b252c8a0952a9
Tatiana <lacustataty@yahoo.com>
2024-03-21 15:24:20 -0400

    Created logout step def class

---
+++ src/test/java/com/twiliaerp/step_definitions/LogOutStepDef_B32G20_136_Tatiana.java
@@ -0,0 +1,20 @@
+package com.twiliaerp.step_definitions;
+
+import io.cucumber.java.en.When;
+
+public class LogOutStepDef_B32G20_136_Tatiana {
+
+    @When("user click account name tab")
+    public void user_click_account_name_tab() {
+
+    }
+    @When("user click log out button")
+    public void user_click_log_out_button() {
+
+    }
+    @When("user should lands on log in page again")
+    public void user_should_lands_on_log_in_page_again() {
+
+    }
+
+}

12927240b9c69995a4f6bf1b10a426a7812f6329
Buse Ayvedi <146566700+buseayvedi@users.noreply.github.com>
2024-03-21 21:11:57 -0400

    update BrowserUtilities.java add sleep method

--- src/test/java/com/twiliaerp/utilities/BrowserUtilities.java
+++ src/test/java/com/twiliaerp/utilities/BrowserUtilities.java
@@ -1,6 +1,17 @@
 package com.twiliaerp.utilities;
 
+import org.junit.experimental.theories.Theories;
+
 public class BrowserUtilities {
+
+    public static void sleep(int seconds){
+
+        try{
+            Thread.sleep(seconds*1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
 
 
 

0d1a3b652731b33c79d0937355cf70f9d898a01f
Buse Ayvedi <146566700+buseayvedi@users.noreply.github.com>
2024-03-23 15:39:08 -0400

    update

--- src/test/java/com/twiliaerp/page/SalesPage_B32G20_141_Buse.java
+++ src/test/java/com/twiliaerp/page/SalesPage_B32G20_141_Buse.java
@@ -4,6 +4,7 @@ import com.twiliaerp.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -30,6 +31,12 @@ public class SalesPage_B32G20_141_Buse extends BasePage_US01_Buse {
 
     public void clickSearchBox(){
         searchBox.click();
+    }
+
+    public void clickSalesModule(){
+        salesBtn.click();
+        wait.until(ExpectedConditions.visibilityOf(quotationNumberTitle));
+
     }
 
 

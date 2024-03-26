5643af3b5276f21a4a924186cc9666cee20354ba
Rana <raldarra@gmail.com>
2024-03-26 01:14:42 -0400

    commit changes

--- src/test/java/com/twiliaerp/runner/CukesRunner.java
+++ src/test/java/com/twiliaerp/runner/CukesRunner.java
@@ -29,7 +29,7 @@ import org.junit.runner.RunWith;
 
 
         // Specifies the tags to include/exclude specific scenarios or features
-        tags = "",
+        tags = "@B32G20-173",
         // Whether to execute a dry run of the feature files
         dryRun = false,
 
